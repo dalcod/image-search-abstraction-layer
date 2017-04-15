996680177536c8dc53bff38abb9c1dfce153beeb
sulphurv <n.dalco@hotmail.it>
2017-04-15 12:46:21 -0700

    final

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-ECHO attivo.
+
--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var bing = require("node-bing-api")({accKey: "6b61248133364cae95330e6bbb463cbb"});
-var url = require("url");
+var mongodb = require("mongodb").MongoClient;
+var url = process.env.MONGOLAB_URI;
 var app = express();
 var port = process.env.PORT || 8080;
+
+/*mongodb.connect(url, function(err, db){
+    if (err) {
+        console.log(err);
+        return;
+    }
+    db.collection("img-entries").remove({});
+    db.close();
+});*/
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
@@ -18,13 +28,28 @@ app.get("/search", function(req, res, next){
     var thatRes = res;
     var searchStr = req.query.q;
     var offset = req.query.offset;
-    if(!searchStr || !offset){
+
+    if (!searchStr || !offset) {
         next();
         return;
     }
-    if(offset > 50){
+    if (offset > 50) {
         res.end("The maximum number of images per offset is 50.");
     }
+
+    mongodb.connect(url, function(err, db){
+        if (err) {
+            console.log(err);
+            return;
+        }
+        var date =  new Date().toString().split(/GMT/);
+        db.collection("img-entries").insert({
+            "entry": searchStr,
+            "when": date[0]
+        });
+        db.close();
+    });
+
     bing.images(searchStr, {top: offset}, function(error, res, body){
         if (error) {
             next();
@@ -41,6 +66,24 @@ app.get("/search", function(req, res, next){
         });
         app.locals.contArr = arr;
         thatRes.render("result");
+    });
+});
+
+app.get("/history", function(req, res, next){
+    mongodb.connect(url, function(err, db){
+        if (err) {
+            console.log(err);
+            return;
+        }
+        db.collection("img-entries").find({}).toArray(function(err, data){
+            if (err) {
+                console.log(err);
+                return;
+            }
+            app.locals.entries = data;
+            res.render("history");
+            db.close();
+        });
     });
 });
 
